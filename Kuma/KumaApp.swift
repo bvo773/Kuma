58a2d8e24533d48c6753d76a25f874bfc7692f62
Binh Vo <binhvo07@gmail.com>
2025-01-25 08:10:01 -0600

    Initial Commit

---
+++ Kuma/KumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KumaApp.swift
+//  Kuma
+//
+//  Created by spaceduck on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct KumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
